7e1dd2222329d6cecb83b9b197d04705aabae3d0
bennntwig <bennntwig@gmail.com>
2021-12-10 02:13:09 +0100

    hasNumber in BingoCard

--- src/com/company/BingoCard.java
+++ src/com/company/BingoCard.java
@@ -10,6 +10,18 @@ public class BingoCard {
         }
     }
 
+    public void hasNumber(int b){
+        boolean returnVar = false;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if(cardNumbers[i][j] == b){
+                    cardMarkers[i][j] == true;
+                    break;
+                }
+            }
+        }
+    }
+
     public void fill(int[] numbers){
         cardNumbers[0][0] = numbers[0];
         cardNumbers[0][1] = numbers[1];
